13877e9714ffa5d4b8fc9b9821d40eb9904a4513
kylewludwig <24880524+kylewludwig@users.noreply.github.com>
2021-03-06 17:07:59 -0800

    Fix mavsdk_server naming issue causing library product loading error

--- Package.swift
+++ Package.swift
@@ -6,11 +6,11 @@ let package = Package(
   name: "MAVSDK-XCFramework",
   products: [
     .library(
-      name: "MavsdkBinary",
-      targets: ["MavsdkBinary"]),
+      name: "mavsdk_server",
+      targets: ["mavsdk_server"]),
   ],
   targets: [
-    .binaryTarget(name: "MavsdkBinary",
+    .binaryTarget(name: "mavsdk_server",
                   url: "https://github.com/mavlink/MAVSDK/releases/download/v0.37.0/mavsdk_server.xcframework.zip",
                   checksum: "7c7c45c4f4ae59a93d6cb5d29d2ccede2424108dc549ce94f7ccd834466de51a")
   ]
